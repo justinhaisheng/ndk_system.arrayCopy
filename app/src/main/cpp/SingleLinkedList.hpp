197ed7bc57dd7c1962b63cde377768997f1ea734
haisheng.lu <haisheng.lu@aispeech.com>
2020-02-09 22:17:51 +0800

    实现简单的单链表 插入与删除

--- app/src/main/cpp/SingleLinkedList.hpp
+++ app/src/main/cpp/SingleLinkedList.hpp
@@ -34,11 +34,54 @@ private:
     Node<E>* head = NULL;
     int len = 0;
     Node<E>* getLastNode();
+    Node<E>* getIndexNode(int index);
 public:
     void push(E e);
     int size();
     E get(int index);
+    void insert(int index,E e);
+    E remove(int index);
 };
+
+template <class E>
+E SingleLinkedList<E>::remove(int index) {
+    assert(index >=0 && index <= len - 1);
+    if (index == 0){
+        Node<E>* temp = head;
+        int result = temp->getValue();
+        head = temp->next;
+        delete(temp);
+        this->len -- ;
+        return result;
+    }else{
+        Node<E>* pre = getIndexNode(index - 1);
+        Node<E>* cur = pre->next;
+        int result = cur->getValue();
+        pre->next = cur->next;
+        delete(cur);
+        this->len -- ;
+        return result;
+    }
+
+}
+
+template <class E>
+void SingleLinkedList<E>::insert(int index, E e) {
+    assert(index>=0 && index <= len);
+    Node<E>* node = new Node<E>(e);
+    if (index == 0){
+        Node<E>* temp = head;
+        head = node;
+        node->next = temp;
+    }else{
+        Node<E>* pre = getIndexNode(index - 1);
+        node->next = pre->next;
+        pre->next = node;
+    }
+    this->len ++ ;
+}
+
+
 
 template <class E>
 Node<E>* SingleLinkedList<E>::getLastNode() {
@@ -48,7 +91,19 @@ Node<E>* SingleLinkedList<E>::getLastNode() {
         temp = temp->next;
     }
     return temp;
+
 }
+
+template <class E>
+Node<E>* SingleLinkedList<E>::getIndexNode(int index) {
+    assert(index>=0 && index < len);
+    Node<E>* temp = head;
+    for (int i = 0; i < index; i++) {
+        temp = temp->next;
+    }
+    return temp;
+}
+
 
 template <class E>
 void SingleLinkedList<E>::push(E e) {
@@ -65,16 +120,13 @@ void SingleLinkedList<E>::push(E e) {
 
 template <class E>
 int SingleLinkedList<E>::size() {
-    return this->size();
+    return this->len;
 }
 
 template <class E>
 E SingleLinkedList<E>::get(int index) {
-    //assert(index>=this->len);
-    Node<E>* temp = this->head;
-    for (int i = 0; i < index; ++i) {
-        temp = temp->next;
-    }
+    assert(index<this->len);
+    Node<E>* temp = getIndexNode(index);
     return temp->getValue();
 }
 
--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -25,11 +25,27 @@ Java_com_aispeech_array_MainActivity_stringFromJNI(
     singleLinkedList.push(5);
     singleLinkedList.push(10);
     singleLinkedList.push(11);
-
-    for (int i = 0; i < 3; ++i) {
+    singleLinkedList.push(33);
+    for (int i = 0; i < singleLinkedList.size(); ++i) {
         int d = singleLinkedList.get(i);
         LOGD("singleLinkedList (%d)",d);
     }
+
+    singleLinkedList.insert(0,100);
+    singleLinkedList.insert(singleLinkedList.size(),101);
+    singleLinkedList.insert(3,55);
+    for (int i = 0; i < singleLinkedList.size(); ++i) {
+        int d = singleLinkedList.get(i);
+        LOGD("singleLinkedList insert之后 (%d)",d);
+    }
+
+   int re0 =  singleLinkedList.remove(0);
+    int re5 =  singleLinkedList.remove(5);
+    for (int i = 0; i < singleLinkedList.size(); ++i) {
+        int d = singleLinkedList.get(i);
+        LOGD("singleLinkedList remove (%d)",d);
+    }
+    LOGD("singleLinkedList re0 = %d re5 = %d",re0,re5);
 
     return env->NewStringUTF(hello.c_str());
 }
