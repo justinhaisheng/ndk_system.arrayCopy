15cb2401414fd6de5c4e80268d7a6c53d8030abd
haisheng.lu <haisheng.lu@aispeech.com>
2020-02-29 15:26:57 +0800

    二叉树进行序列化

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -82,6 +82,22 @@ bool isBalanceTree(TreeNode<char> *pNode) {
     return abs(left - right) <=1 && isBalanceTree(pNode->left) && isBalanceTree(pNode->right);
 }
 
+void serialized(TreeNode<char> *pNode,char s[],int &index){
+    if (!pNode){
+        s[index]= '#';
+        LOGI("serialized # index=%d",index);
+        index = index+1;
+        //s++;
+        return;
+    }
+    s[index] = pNode->data;
+    LOGI("serialized %c index=%d",pNode->data,index);
+    index = index+1;
+    //s++;
+    serialized(pNode->left,s,index);
+    serialized(pNode->right,s,index);
+}
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_aispeech_array_MainActivity_stringFromJNI(
         JNIEnv* env,
@@ -169,8 +185,28 @@ Java_com_aispeech_array_MainActivity_stringFromJNI(
 
     int count = getDepthTree(root);
     bool  ba = isBalanceTree(root);
-    
-
     LOGE("二叉树 count%d %d",count,ba);
+
+    LOGD("序列化和反序列化");
+    TreeNode<char>* serializeA = new TreeNode<char>('A');
+    TreeNode<char>* serializeB = new TreeNode<char>('B');
+    TreeNode<char>* serializeC = new TreeNode<char>('C');
+    TreeNode<char>* serializeD = new TreeNode<char>('D');
+    TreeNode<char>* serializeE = new TreeNode<char>('E');
+    TreeNode<char>* serializeF = new TreeNode<char>('F');
+
+    serializeA->left = serializeB;
+    serializeA->right = serializeC;
+    serializeB->left = serializeD;
+    serializeB->right = serializeE;
+    serializeC->right = serializeF;
+
+    char* serialize = static_cast<char *>(malloc(sizeof(char) * 30));
+    int index = 0;
+    serialized(serializeA,serialize,index);
+    //preOrderTraverse(serializeA);
+    LOGE("serialize= %s",serialize);
+    free(serialize);
+
     return env->NewStringUTF(hello.c_str());
 }
